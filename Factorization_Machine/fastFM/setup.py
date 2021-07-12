ce666d171e1bd2c08d423742fa78a974e79b6503
K-W-hibiki <63836048+K-W-hibiki@users.noreply.github.com>
2021-07-12 21:08:11 +0900

    main

--- Factorization_Machine/fastFM/setup.py
+++ Factorization_Machine/fastFM/setup.py
@@ -1,15 +1,13 @@
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
-from numpy import get_include # cimport numpy を使うため
+#from distutils.core import setup, Extension
+##from Cython.Build import cythonize
+#from numpy import get_include # cimport numpy を使うため
+#ext = Extension("ffm", sources=["ffm.pyx"], include_dirs=['.', get_include()])
+#setup(name="ffm", ext_modules=cythonize([ext]))
 
-ext = Extension("ffm", sources=["ffm.pyx"], include_dirs=['.', get_include()])
-setup(name="ffm", ext_modules=cythonize([ext]))
-
-#from distutils.core import setup
-#from distutils.extension import Extension
-#from Cython.Distutils import build_ext
-
-#setup(
-#    cmdclass = {'build_ext': build_ext},
-#    ext_modules = [Extension("ffm", ["ffm.pyx"])]
-#)
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+setup(
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = [Extension("ffm", ["ffm.pyx"])]
+)
